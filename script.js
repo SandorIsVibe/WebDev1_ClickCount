f2aa0ea4e2f1cf39f63549b915216dda3583ebbf
SandorIsVibe <130621127+SandorIsVibe@users.noreply.github.com>
2023-04-12 21:32:27 -0700

    keep the count of click intact after page refresh

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 var count = 0;
 var clickButton = document.getElementById("clickButton");
 var clickCount = document.getElementById("clickCount");
+var log = document.getElementById("myLog");
 
-clickButton.addEventListener("click", function () {
+count = localStorage.getItem("myClickCount")
+clickCount.innerHTML = count;   
+
+function eventFunction() {
+    
+    //it will disappear if I refresh the page
+    log.innerHTML = "event triggered"
+
+    //it will not disappear as it is taking value from localStorage
     count++;
+    localStorage.setItem("myClickCount", count)
     clickCount.innerHTML = count;
-});
+}
+clickButton.addEventListener("click", eventFunction);
+
